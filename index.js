61d51355eddc2c78d04d497977e27d4818b6b4c1
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-05-24 23:02:38 +0600

    Initializing

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const dotenv = require("dotenv");
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    //api
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
